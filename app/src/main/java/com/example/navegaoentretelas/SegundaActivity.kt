4693be417fa963b903613469a7f2f6a1a36f300b
Mayconlucas <lmike.infos@gmail.com>
2024-07-16 19:29:30 -0300

    Navegação entre telas

---
+++ app/src/main/java/com/example/navegaoentretelas/SegundaActivity.kt
@@ -0,0 +1,29 @@
+package com.example.navegaoentretelas
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SegundaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segunda)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val navTelaSegunda = findViewById<Button>(R.id.navTelaSegunda)
+        navTelaSegunda.setOnClickListener {
+            val navPrimeiraTela = Intent(this, MainActivity::class.java)
+            startActivity(navPrimeiraTela)
+        }
+
+    }
+}
